cfedab861f8884bf56fbe58b61b586a561251c28
Ranit Mondal <mondalranit2002@gmail.com>
2025-10-06 12:08:09 +0530

    adding bellman ford

--- shortestpath.py
+++ shortestpath.py
@@ -1,4 +1,4 @@
-# dijkstra algorithm - single source shortest path
+# # dijkstra algorithm - single source shortest path
 # undirected graph
 import heapq
 n=5 # no of nodes
@@ -21,4 +21,24 @@ while len(heap)>0:
         if dist[u]+w< dist[v]:
             dist[v]=dist[u]+w
             heapq.heappush(heap,(dist[v],v))
+print(dist)
+
+
+# bellman ford algorithm- single source shortest path
+n=5 # no of nodes
+edges=[(0,1,9),(0,2,75),(1,2,95),(1,3,19),(1,4,42),(2,3,51),(2,4,66),(3,4,31)]
+# no need of priority Queue and adjacency list
+dist=[float('inf')]*n
+s=0
+dist[s]=0
+# just relax v-1 times , v is no of nodes/vertices
+for i in range(n-1):
+    for u,v,w in edges:
+        if dist[u]+w < dist[v]:
+            dist[v]=dist[u]+w
+# negative cycle check
+for u,v,w in edges:
+    if dist[u]+w< dist[v]:
+        print("negative cycle present")
+        break
 print(dist)
